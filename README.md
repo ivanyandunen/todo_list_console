64871f44607987580808ae86b348cb0b60f095ec
Ivan Yandunen <34913063+ivanyandunen@users.noreply.github.com>
2018-08-02 11:44:54 +0300

    renames variable list to list_of_items

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ You can add ToDo, ToBuy and ToRead items in list
 
 Python 3.5 and higher has to be installed in your system.
 Just run:
+
 ```commandline
 python todo.py
 ```
+
  and have fun.
 
 # Project Goals
--- todo.py
+++ todo.py
@@ -20,38 +20,38 @@ def choose_item():
     return selected
 
 
-def add_new_item(list, item_type):
+def add_new_item(list_of_items, item_type):
     if item_type == '0':
         new_item = input('Input heading: ')
-        list.append('ToDo: ' + new_item)
+        list_of_items.append('ToDo: ' + new_item)
         print('Added ToDo: {}'.format(new_item))
     elif item_type == '1':
         new_item = input('Input heading: ')
         price = input('Input price: ')
-        list.append('ToBuy: ' + new_item + ' for ' +price)
+        list_of_items.append('ToBuy: ' + new_item + ' for ' +price)
         print('Added ToBuy: {} for {}'.format(new_item, price))
     elif item_type == '2':
         new_item = input('Input header: ')
         url = input('Input URL: ')
-        list.append('ToRead: ' + new_item + ' here ' + url)
+        list_of_items.append('ToRead: ' + new_item + ' here ' + url)
         print('Added ToRead {} here {}'.format(new_item, url))
-    return list
+    return list_of_items
 
 
-def print_list(list):
-    if not list:
+def print_list(list_of_items):
+    if not list_of_items:
         print('There are no items in storage.')
     else:
-        for i in range(len(list)):
-            print('{}: {}'.format(i, list[i]))
+        for i in range(len(list_of_items)):
+            print('{}: {}'.format(i, list_of_items[i]))
 
 
 if __name__ == '__main__':
-    list = []
+    list_of_items = []
     action = choose_action()
     while action != 'exit':
         if action == 'new':
-            add_new_item(list, choose_item())
+            add_new_item(list_of_items, choose_item())
         elif action == 'list':
-            print_list(list)
+            print_list(list_of_items)
         action = choose_action()
